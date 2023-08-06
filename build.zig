a09cf6bed7f9f8969512c9f8b69585900a3a9d75
eNV25 <env252525@gmail.com>
2023-08-06 12:28:27 +0200

    Use addInstallFileWithDir instead of custom Step

--- build.zig
+++ build.zig
@@ -4,13 +4,15 @@ pub fn build(b: *std.build.Builder) !void {
     const target = b.standardTargetOptions(.{});
     const mode = b.standardReleaseOptions();
 
-    const lib = b.addSharedLibrary("zig-mpv-plugin", "src/plugin.zig", .unversioned);
-    lib.linker_allow_shlib_undefined = true;
-    lib.linkLibC();
-    lib.setTarget(target);
-    lib.setBuildMode(mode);
+    const plugin = b.addSharedLibrary("zig-mpv-plugin", "src/plugin.zig", .unversioned);
+    plugin.linker_allow_shlib_undefined = true;
+    plugin.linkLibC();
+    plugin.setTarget(target);
+    plugin.setBuildMode(mode);
 
-    _ = installArtifact(b, lib, .prefix, "zig-mpv-plugin.so");
+    const plugin_install = b.addInstallFileWithDir(plugin.getOutputSource(), .lib, "zig-mpv-plugin/plugin.so");
+    plugin_install.step.dependOn(&plugin.step);
+    b.getInstallStep().dependOn(&plugin_install.step);
 
     const main_tests = b.addTest("src/plugin.zig");
     main_tests.setTarget(target);
@@ -19,35 +21,3 @@ pub fn build(b: *std.build.Builder) !void {
     const test_step = b.step("test", "Run library tests");
     test_step.dependOn(&main_tests.step);
 }
-
-fn installArtifact(b: *std.build.Builder, source: *std.build.LibExeObjStep, install_dir: std.build.InstallDir, dest_rel_path: []const u8) *ArtifactInstallStep {
-    const self = b.allocator.create(ArtifactInstallStep) catch unreachable;
-    self.* = .{
-        .step = std.build.Step.init(.custom, "install library, executable or object to custom location", b.allocator, ArtifactInstallStep.make),
-        .builder = b,
-        .source = source.getOutputSource(),
-        .install_dir = install_dir,
-        .dest_rel_path = b.allocator.dupe(u8, dest_rel_path) catch unreachable,
-    };
-    self.step.dependOn(&source.step);
-    b.getInstallStep().dependOn(&self.step);
-    return self;
-}
-
-const ArtifactInstallStep = struct {
-    step: std.build.Step,
-
-    builder: *std.build.Builder,
-
-    source: std.build.FileSource,
-    install_dir: std.build.InstallDir,
-    dest_rel_path: []const u8,
-
-    fn make(step: *std.build.Step) !void {
-        const self = @fieldParentPtr(ArtifactInstallStep, "step", step);
-        const b = self.builder;
-        const source_path = self.source.getPath(b);
-        const dest_path = b.getInstallPath(self.install_dir, self.dest_rel_path);
-        try b.updateFile(source_path, dest_path);
-    }
-};
