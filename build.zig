64bff6b6b2e68ba75a1ac3bbd9cb8898cddfd6b1
eNV25 <env252525@gmail.com>
2023-08-08 13:49:08 +0200

    Cleanup build.zig

--- build.zig
+++ build.zig
@@ -1,25 +1,28 @@
 const std = @import("std");
 
-pub fn build(b: *std.build.Builder) !void {
+pub fn build(b: *std.Build.Builder) !void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
     const cc = b.option(bool, "cc", "Use C compiler with Zig's C backend") orelse false;
 
-    const root_source_file = .{ .path = "src/plugin.zig" };
+    const ctarget = ctarget: {
+        var ctarget = target;
+        ctarget.ofmt = .c;
+        break :ctarget ctarget;
+    };
 
-    const cplugin = b.addSharedLibrary(.{
-        .name = "plugin",
-        .root_source_file = root_source_file,
-        .target = target,
+    const plugin_zig_source_file = std.Build.LazyPath{ .path = "src/plugin.zig" };
+    const plugin_source_file = if (cc) b.addObject(.{
+        .name = "cplugin",
+        .root_source_file = plugin_zig_source_file,
+        .target = ctarget,
         .optimize = optimize,
         .link_libc = true,
-    });
-    cplugin.target.ofmt = .c;
-    cplugin.out_filename = "plugin.c";
+    }).getEmittedBin() else plugin_zig_source_file;
 
     const plugin = b.addSharedLibrary(.{
         .name = "plugin",
-        .root_source_file = if (cc) cplugin.getEmittedBin() else root_source_file,
+        .root_source_file = plugin_source_file,
         .target = target,
         .optimize = optimize,
         .link_libc = true,
