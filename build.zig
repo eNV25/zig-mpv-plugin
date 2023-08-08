4cf5cc056387b76ccebc18bd39571e7491649cbe
eNV25 <env252525@gmail.com>
2023-08-08 11:11:13 +0200

    Port to zig 0.11.0

--- build.zig
+++ build.zig
@@ -2,22 +2,37 @@ const std = @import("std");
 
 pub fn build(b: *std.build.Builder) !void {
     const target = b.standardTargetOptions(.{});
-    const mode = b.standardReleaseOptions();
+    const optimize = b.standardOptimizeOption(.{});
+    const cc = b.option(bool, "cc", "Use C compiler with Zig's C backend") orelse false;
 
-    const plugin = b.addSharedLibrary("zig-mpv-plugin", "src/plugin.zig", .unversioned);
+    const root_source_file = .{ .path = "src/plugin.zig" };
+
+    const cplugin = b.addSharedLibrary(.{
+        .name = "plugin",
+        .root_source_file = root_source_file,
+        .target = target,
+        .optimize = optimize,
+        .link_libc = true,
+    });
+    cplugin.target.ofmt = .c;
+    cplugin.out_filename = "plugin.c";
+
+    const plugin = b.addSharedLibrary(.{
+        .name = "plugin",
+        .root_source_file = if (cc) cplugin.getEmittedBin() else root_source_file,
+        .target = target,
+        .optimize = optimize,
+        .link_libc = true,
+    });
     plugin.linker_allow_shlib_undefined = true;
-    plugin.linkLibC();
-    plugin.setTarget(target);
-    plugin.setBuildMode(mode);
 
-    const plugin_install = b.addInstallFileWithDir(plugin.getOutputSource(), .lib, "zig-mpv-plugin/plugin.so");
-    plugin_install.step.dependOn(&plugin.step);
-    b.getInstallStep().dependOn(&plugin_install.step);
+    b.getInstallStep().dependOn(&b.addInstallLibFile(plugin.getEmittedBin(), "zig-mpv-plugin/plugin.so").step);
 
-    const main_tests = b.addTest("src/plugin.zig");
-    main_tests.setTarget(target);
-    main_tests.setBuildMode(mode);
-
-    const test_step = b.step("test", "Run library tests");
-    test_step.dependOn(&main_tests.step);
+    const tests = b.step("test", "Run tests");
+    tests.dependOn(&b.addTest(.{
+        .root_source_file = root_source_file,
+        .target = target,
+        .optimize = optimize,
+        .link_libc = true,
+    }).step);
 }
--- src/plugin.zig
+++ src/plugin.zig
@@ -8,13 +8,13 @@ export fn mpv_open_cplugin(ctx: *C.mpv_handle) c_int {
     _ = C.mpv_hook_add(ctx, 0, "on_load", 11);
 
     while (true) {
-        const ev = C.mpv_wait_event(ctx, -1).*;
+        const ev: *C.mpv_event = C.mpv_wait_event(ctx, -1);
         switch (ev.event_id) {
             C.MPV_EVENT_SHUTDOWN => {
                 return 0;
             },
             C.MPV_EVENT_HOOK => {
-                const hook = @ptrCast(*C.mpv_event_hook, @alignCast(@alignOf(*C.mpv_event_hook), ev.data));
+                const hook: *C.mpv_event_hook = @ptrCast(@alignCast(ev.data));
 
                 if (mem.eql(u8, mem.span(hook.name), "on_load")) {
                     const url = C.mpv_get_property_string(ctx, "stream-open-filename");
